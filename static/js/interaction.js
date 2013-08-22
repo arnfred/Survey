fd2230d3fe0e5b6c3f66b599abf8f426410ff5b6
Jonas Arnfred <jonas@ifany.org>
2013-08-22 03:20:26 +0000

    Added error checking

--- static/js/interaction.js
+++ static/js/interaction.js
@@ -4,9 +4,17 @@ $(function() {
     $( "#step1" ).click(step2)
     $( "#step2" ).click(step3)
     $( "#step3" ).click(step4)
+
+    // Remove error box when we click on a radio field
+    $( "input[type=radio]" ).each(function(i,f) { $(f).click(function() { 
+        $(f).parents(".question").removeClass("error"); 
+    }); });
 });
 
 function step2(e) {
+
+    // Check that the form has been filled out and if not, abort
+    if (step2CheckErrors() == false) return false
 
     // Make images selectable
     $( "#images li" ).each(function (index,e) { $(e).click(function(event,li) { $(e).toggleClass("selected"); }) })
@@ -17,9 +25,20 @@ function step2(e) {
         $( "#images" ).fadeIn("fast");
     });
 
+    // Clear error messages
+    $("#errorMsg").html("");
+
+    return true;
 }
 
 function step3(e) {
+
+    // Check that we have at least selected some images
+    if ($(".selected").length < 4) {
+        $("#errorMsg").append("You need to select more images<br/>");
+        return false;
+    }
+    
     // Fade out not selected images
     $( "#images li" ).not(".selected").fadeOut("fast")
     $( ".step2" ).fadeOut("fast", function () { 
@@ -29,6 +48,35 @@ function step3(e) {
     // Make the rest sortable
     $( "#images" ).sortable(); 
 
+    // Clear error messages
+    $("#errorMsg").html("");
+
     return false
 }
 
+function step4(e) {
+    // Collect data
+
+    return false
+}
+
+function step2CheckErrors() {
+
+    // Verify that form fields have been filled out
+    var groups = $.makeArray($("input:radio").map(function(i,e) { return $(e).attr("name"); })).unique();
+    console.debug(groups)
+    // For each group, check that there is a buttom that has been checked
+    var tally = groups.map(function(n,i) { 
+        var field = $("input[name=" + n + "]"); 
+        var noError = true;
+        if (!field.is(":checked")) { 
+            field.parents(".question").addClass("error"); 
+            $("#errorMsg").append("Error in question #" + (i+1) + "<br/>");
+            noError = false;
+        }
+        return noError;
+    })
+
+    // If we have any fields where there were errors, don't proceed
+    return tally.every(function(a) { return a });
+}
--- survey.py
+++ survey.py
@@ -1,47 +1,38 @@
-# Main instance of survey
-
-import web
-import os
-from web import form
-
-# Set up stuff for web server
-urls = (
-    '/', 'survey'
-)
-render = web.template.render('templates/')
-app = web.application(urls, globals())
-
-# Set up form for survey
-survey_form = form.Form( 
-    form.Radio("devices", [(1,'just 1'),(2,'2 - 3'),(3,'4 or more')], form.notnull, description="How many cameras do you own?"),
-    form.Radio("trick", [('yes', 'I sure am'),('no', 'I would never!')], description="Are you just making up answers to get a quick payment?", value='yes')
-)
-
-# Set up web page text fields
-class text_object(object):
-    pass
-text = text_object()
-text.title = "Image Survey"
-text.step1_desc = "Please answer a few questions before we start ... "
-text.step2_desc = "Select all images that you want in your photo album"
-text.step3_desc = "Drag and drop images to order them the way you'd like them to be ordered in a slide show"
-
-# This is such a cheap hack, but some times global state is just easier
-currentFolderIndex = 0
-def cycleFolder() :
-    global currentFolderIndex
-    currentFolderIndex = (currentFolderIndex + 1) % 3
-    folders = ["Beach", "Bird", "Other"]
-    return "images/%s/" % folders[currentFolderIndex]
-
 # Handler for root path
 class survey:        
-    def GET(self):
+    currentFolderIndex = 0
+
+    def GET(self) :
+
+        # Set up web page form
+        survey_form = form.form()
+        survey_form.question("devices", [(1,'just 1'),(2,'2 - 3'),(3,'4 or more')], "How many cameras do you own?")
+        survey_form.question("devices2", [(1,'just 1'),(2,'2 - 3'),(3,'4 or more')], "How many cameras do you own?")
+        survey_form.question("trick", [('yes', 'I sure am'),('no', 'I would never!')], "Are you just making up answers to get a quick payment?", "yes")
+
+        # Set up page text
+        text = text_object()
+        text.title = "Image Survey"
+        text.step1_desc = "Please answer a few questions before we start:"
+        text.step2_desc = "Select all images that you want in your photo album"
+        text.step3_desc = "Drag and drop images to order them the way you'd like them to be ordered in a slide show"
 
         # Get images in imagefolder
         imgdir = cycleFolder()
         imgs = ["/static/%s/%s" % (imgdir, i) for i in os.listdir("static/%s" % imgdir)]
         return render.survey(imgs, text, survey_form)
 
-if __name__ == "__main__":
-    app.run()
+
+    def POST(self) :
+        pass
+
+
+    # Switch between folders on each reload
+    def cycleFolder() :
+        self.currentFolderIndex = (self.currentFolderIndex + 1) % 3
+        folders = ["Beach", "Bird", "Other"]
+        return "images/%s/" % folders[self.currentFolderIndex]
+
+# Set up web page text fields
+class text_object(object):
+    pass
