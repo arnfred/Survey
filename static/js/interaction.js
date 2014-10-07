736f50a2139f241aa448dd7e9ba8190557d31a2d
jonas arnfred <jonas@ifany.org>
2014-10-07 03:54:13 +0000

    Getting repo up to date

--- static/js/interaction.js
+++ static/js/interaction.js
@@ -1,70 +1,222 @@
 $(function() {
 
-    // Go to step 2 when button is clicked
-    $( "#step1" ).click(step2)
-    $( "#step2" ).click(step3)
-    $( "#step3" ).click(step4)
+    // Get init timestamp
+    var timestamps = [];
+	var currentStep = ["intro1"]
+    addTimeStamp(timestamps, currentStep[0])
+
+    // Show first intro text
+    $( "." + currentStep[0] ).fadeIn("fast");
+
+    // Go to next section when we click on nav button
+    $( ".next" ).on("click", function () { 
+        return next($(this).attr("id"), timestamps, currentStep); 
+    });
+
+    // Make images selectable
+    images()
 
     // Remove error box when we click on a radio field
-    $( "input[type=radio]" ).each(function(i,f) { $(f).click(function() { 
+    $( "input[type=radio]" ).each(function(i,f) { $(f).on("click", function() { 
         $(f).parents(".question").removeClass("error"); 
     }); });
 });
 
-function step2(e) {
 
-    // Check that the form has been filled out and if not, abort
-    if (step2CheckErrors() == false) return false
+function next(step, timestamps, currentStep) {
+
+    // Add timestamp
+    addTimeStamp(timestamps, currentStep[0])
+
+    // Check that the form is filled out properly
+    if (step == "step1" && !formCheckErrors()) {
+        return false;
+    }
+
+    // Check that enough images have been selected
+    if (step == "step2") {
+		if (!ImagesCheckErrors()) {
+			return false;
+		}
+		else {
+			ImagesMakeSortable();
+		}
+    }
+
+    // Save data and furnish code
+    if (step == "done") {
+		if (!sortingCheckErrors()) {
+			return false;
+		}
+        furnishData(timestamps)
+    }
+
+    // Hide old
+    $(".section").has(":visible").fadeOut("fast");
+    
+    // Show new
+    $("." + step).fadeIn("fast");
+	
+	// Update currentStep
+	currentStep[0] = step
+
+    // Cancel normal click event
+    return false;
+
+}
+
+
+function images() {
 
     // Make images selectable
-    $( "#images li" ).each(function (index,e) { $(e).click(function(event,li) { $(e).toggleClass("selected"); }) })
+    $( "#images li" ).each(function (index,e) { $(e).on("click",function(event,li) { $(e).toggleClass("selected"); }) })
 
-    // Fade out form and then fade in step2 and images
-    $( ".step1" ).fadeOut("fast", function() {
-        $( ".step2" ).fadeIn("fast");
-        $( "#images" ).fadeIn("fast");
-    });
+    // Make bigger image appear
+    $("li").each(function(i, li) { $(li).mouseover(function(e) { 
+        $("div#zoom img").attr('src',$(li).children("img").attr("src").replaceLast("/","/large/")); 
+    }); });
+}
+
+
+function addTimeStamp(timestamps, eventname) {
+    timestamps.push({ "name" : eventname, "time" : Math.round(new Date().getTime() / 1000) });
+}
+
+
+function ImagesCheckErrors() {
 
     // Clear error messages
     $("#errorMsg").html("");
-
-    return true;
-}
-
-function step3(e) {
 
     // Check that we have at least selected some images
     if ($(".selected").length < 4) {
         $("#errorMsg").append("You need to select more images<br/>");
         return false;
     }
-    
-    // Fade out not selected images
-    $( "#images li" ).not(".selected").fadeOut("fast")
-    $( ".step2" ).fadeOut("fast", function () { 
-        $( ".step3" ).fadeIn("fast")
-    })
+	else if ($("#question_pick").val() == "") {
+        $("#errorMsg").append("You need to add text in the text area<br/>");
+        return false;
+    }
+    else {
+		return true
+	}
+}
 
-    // Make the rest sortable
-    $( "#images" ).sortable(); 
-
-    // Clear error messages
+function sortingCheckErrors() {
+	// Clear error messages
     $("#errorMsg").html("");
 
-    return false
+    // Check that we have some text in the box
+    if ($("#question_order").val() == "") {
+        $("#errorMsg").append("You need to add text in the text area<br/>");
+        return false;
+    }
+    else {
+		return true
+	}
 }
 
-function step4(e) {
-    // Collect data
+function ImagesMakeSortable() {
+	// Store how many clicks
+	clicks = 0 // Yes, it's global. Sorry.
+	// Make images sortable
+	$( "#images ol" ).sortable({ tolerance: "pointer", items: ".item" }); 
+	// Fade out not selected images
+	$( "#images li" ).not(".selected, .endings").remove();
+	// remove border
+	$( "#images li" ).removeClass("selected")
+	// remove click event
+	$( "#images li" ).each(function (index,e) { $(e).off() });
+	// count clicks
+	$( "#images li" ).each(function (index,e) { $(e).on("mousedown", function() {
+		clicks += 1;
+	}) });
+	// Set width to full page
+	$( "#images ol" ).css("width","90%"); 
+	// Resize all images to half size
+	//$( "#images li" ).css("width", "150px");
+	$( "li.item img" ).each(function(i, img) { 
+		//$(img).css("width", $(img).width()/2.0);
+		$(img).css("cursor","move");
+	});
+	// increasing margin
 
-    return false
+	// Show start and end list items
+	$( ".endings" ).fadeIn("fast");
+
 }
 
-function step2CheckErrors() {
+
+function furnishData(timestamps) {
+
+    // Collect data from questions
+    var groups = $.makeArray($("input:radio").map(function(i,e) { return $(e).attr("name"); })).unique();
+    questions = groups.map(function(n,i) { return { "name" : n, "value" : $("input[name=" + n + "]:checked").val() } }); 
+
+    // Collect order of photos and collection name
+    path_len = $("li.item img").filter(":visible").attr("src").split('/').length
+    photos = $.makeArray($("li img").filter(":visible").map(function(i,im) { return $(im).attr("src").split('/')[path_len - 1]; }))
+    collection = $("li.item img").filter(":visible").attr("src").split('/')[path_len - 2];
+
+    // Collect timestamps
+    time_diffs = timestamps.map(function(t,i) { if (i == 0) return { "name" : t.name, "time" : 0 }; else return { "name" : t.name, "time" : t.time - timestamps[i-1].time }; } );
+
+    // Assemble data
+    survey_data = {
+        "questions" : questions,
+        "photo_order" : photos,
+        "collection" : collection,
+        "time_diffs" : time_diffs,
+		"clicks" : clicks, // global variable defined in makeImagesSortable
+		//"begin_time" : timestamps[0],
+		"question_pick" : $("#question_pick").val(),
+		"question_order" : $("#question_order").val(),
+		"window_x" : $(window).width(),
+		"window_y" : $(window).height(),
+		"mw_id" : getParameterByName("mw_id")
+		
+    }
+
+    // Send data to server
+    promise = $.post( "/", JSON.stringify(survey_data), function(data, textStatus, jqXHR) {
+        //console.debug(data)
+    }, "json");
+
+    // Wait for response to show step5
+    promise.complete(showVoucher)
+}
+
+
+function showVoucher(data) {
+
+    // Fade out the waiting text
+    $("#waiting").fadeOut("fast");
+
+    // If the result was positive
+    if (data.status == 200) {
+
+        // Show voucher
+        $("#voucher").html(data.responseText);
+        $("#success").fadeIn("fast");
+    } 
+    // If the result was a failure
+    else {
+        $("#failure").fadeIn("fast");
+    }
+
+}
+
+
+
+
+
+function formCheckErrors() {
+
+    // Clear the error messages from last run
+    $("#errorMsg").html("");
 
     // Verify that form fields have been filled out
     var groups = $.makeArray($("input:radio").map(function(i,e) { return $(e).attr("name"); })).unique();
-    console.debug(groups)
     // For each group, check that there is a buttom that has been checked
     var tally = groups.map(function(n,i) { 
         var field = $("input[name=" + n + "]"); 
@@ -79,4 +231,15 @@ function step2CheckErrors() {
 
     // If we have any fields where there were errors, don't proceed
     return tally.every(function(a) { return a });
+}
+
+
+
+// Thanks to dextOr at 
+// http://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
--- survey.py
+++ survey.py
@@ -1,38 +1,155 @@
+""" Small backend for survey page """
+
+import os
+import web
+import json
+import csv
+import random
+from string import ascii_uppercase, digits
+import form
+import hashlib
+import time
+
 # Handler for root path
 class survey:        
-    currentFolderIndex = 0
+    render = web.template.render('templates/')
 
     def GET(self) :
+	
+		# Get images in imagefolder
+        imgdir = cycleFolder()
 
         # Set up web page form
         survey_form = form.form()
-        survey_form.question("devices", [(1,'just 1'),(2,'2 - 3'),(3,'4 or more')], "How many cameras do you own?")
-        survey_form.question("devices2", [(1,'just 1'),(2,'2 - 3'),(3,'4 or more')], "How many cameras do you own?")
-        survey_form.question("trick", [('yes', 'I sure am'),('no', 'I would never!')], "Are you just making up answers to get a quick payment?", "yes")
+        survey_form.question("age", [('0to10','Under 10 years old'),('10to20','Between 10 and 19'),('20to30','Between 20 and 29'), ('30to40','Between 30 and 39'), ('over40', 'Over 40')], "How old are you?")
+        survey_form.question("gender", [('male','Male'),('female','Female'),('other', 'Other')], "What is your gender?")
+        survey_form.question("nb_devices", [(0,'None'),(1,'One'),(2,'More than one')], "How many cameras do you own?")
+        survey_form.question("smartphone_p", [('yes','Yes'),('no','No')], "Do you own a smart-phone?")
+        survey_form.question("nb_years", [("0to5","Less than 5 years ago"),("5to10",'Between 5 and 10 years'),("10plus", "More than 10 years ago"),("NA","I don't have a camera")], "How many years approximately is it since you had your first digital camera?")
+        survey_form.question("nb_images_day", [("0to20","Less than 20"),("20to100",'Between 20 and 100 photos'), ("over100", "More than 100")], "How many images approximately do you take on average every month?")
+        survey_form.question("nb_images_library", [("0to1000","Less than 1000"),("1000to5000",'Between 1000 and 5000 photos'),("over5000",'Over 5000 photos')], "How many images approximately does your photo library have?")
+        survey_form.question("verify", [('yes', 'Yes'),('no', 'No')], "Did you provide false answers in the previous questions?")
 
         # Set up page text
-        text = text_object()
-        text.title = "Image Survey"
-        text.step1_desc = "Please answer a few questions before we start:"
-        text.step2_desc = "Select all images that you want in your photo album"
-        text.step3_desc = "Drag and drop images to order them the way you'd like them to be ordered in a slide show"
+        sections = page_object()
+        sections.page_title = "Slideshow Survey"
+        sections.keys = ["intro1", "intro2", "intro3", "step1", "step2", "done"]
 
-        # Get images in imagefolder
-        imgdir = cycleFolder()
-        imgs = ["/static/%s/%s" % (imgdir, i) for i in os.listdir("static/%s" % imgdir)]
-        return render.survey(imgs, text, survey_form)
+        # Title for each step
+        sections.title = {
+                "intro1" : "Slideshow Survey",
+                "intro2" : "Description of the task",
+                "intro3" : "Personal questions",
+                "step1" : "Step1: Select photos",
+                "step2" : "Step2: Sequence photos",
+                "done" : "Finished",
+                }
+				
+        def step1_select() :
+            d = "unassigned"
+            print(imgdir)
+            if imgdir == "images/baby" :
+                d = """This is Josh. You want to make a slideshow about him. Imagine that he is your son/younger brother etc. Select any images that you would like to include in the slideshow by clicking on them (a red border will appear). There is no restriction on the number of images that you can select. Then explain in the textbox why you selected these particular images."""
+            elif imgdir == "images/girl" :
+                d = """This is Jess. You want to make a slideshow about her. Imagine that she is your daughter/sister etc. Select any images that you would like to include in the slideshow by clicking on them (a red border will appear). There is no restriction on the number of images that you can select. Then explain in the textbox why you selected these particular images."""
+            elif imgdir == "images/female" :
+                d = """This is Jill. You want to make a slideshow about her. Imagine that she is your best friend/girlfriend/ sister/daughter etc. Select any images that you would like to include in the slideshow by clicking on them (a red border will appear). There is no restriction on the number of images that you can select. Then explain in the textbox why you selected these particular images."""
+            elif imgdir == "images/male" :
+                d = """This is Jack. You want to make a slideshow about him. Imagine that he is your best friend/boyfriend/ brother/son etc. Select any images that you would like to include in the slideshow by clicking on them (a red border will appear). There is no restriction on the number of images that you can select. Then explain in the textbox why you selected these particular images."""
+            elif imgdir == "images/couple" :
+                d = """This is Jack and Jill. You want to make a slideshow about them. Imagine that they are your best friends. Select any images that you would like to include in the slideshow by clicking on them. Selected images will have a red border around them (a red border will appear). There is no restriction on the number of images that you can select. Then explain in the textbox why you selected these particular images."""
+            print(d)
+            return d
+
+        # Description for each step
+        sections.desc = {
+                "intro1" : """<p>Welcome to ADSC's slideshow creation survey! This is an experiment conducted by the PhotoWork group.</p>
+<p>You will be shown 60 photos of a person and you will be asked to create a slideshow about him/her. You will have to select any number of photos that you like and you will have to arrange them into a slideshow, about this particular person.<br/><br/>
+We expect you to spend 10 to 15 minutes for this task. If the quality of your work is adequate, you will be awarded 0.2$ from the Microworkers crowdsourcing platform. If you are really committed and the quality of your work is very good, you will get a BONUS of 0.2$ (total 0.4$).</p>
+<p>Click NEXT for a thorough description of the task.</p>""",
+
+				"intro2" : """<img src="/static/description.jpg" />""",
+                "intro2-bak" : """<p>Your aim is to <span class='underline'>create a slideshow for a specific person</span>. <br/><br/>
+Imagine that <span class='underline'>this person is important toy you</span> (e.g. your best friend, girlfriend/boyfriend, daughter/son etc.) and you would like to create a slideshow about him/her.<br/><br/>
+Think about the following scenario: Some friends are visiting and you would like to show them photos of this person, so, you compile a slideshow about him/her.<br/><br/>
+The task has 3 sections:</p>
+<img class="content" src='/static/pic.png'/>
+<h4>Step1: Select photos</h2>
+<p>You will be presented with 60 images. All images will portray a specific person (or a couple) in various moments or activities. Your aim is to <span class='underline'>choose any</span> images that you think are appropriate to be used in a slideshow. There is no restriction on the number of images that you can select. Select any photos that you like by simply clicking on them. A <span class='red'>red border</span> will appear around each image that you have clicked. This means that it is selected. If you click again on them, you will deselect them and the <span class="red">red border</span> will disappear. <span class='bold'>Remember that your aim is to use these photos in order to create a slideshow for this person</span>. Once you are ready, click Next.</p>
+<h4>Step2: Sequence photos</h2>
+<p>You will be presented with the images that you selected in Step1. Your aim is to <span class='underline'>rearrange the photos into a sequence</span> that you think is good for describing the person depicted. You can just drag the images and rearrange their order. Remember that your aim is to create a good sequence (according to your personal taste) that is appropriate for representing this particular person. Once you are done, click Next</p>
+<h4>Step3: Finished!</h2>
+<p>Once you have finished, a unique code will be generated for you. Write down this code! You will need it in order to get paid for the task that you have just performed. Go to the MicroWorkers website and use this code in order to redeem your 1$ payment!</p>
+<p>If you are ready, click Next and the test will begin!</p>""",
+
+                "intro3" : """<p>Before you will continue to the test, we would like to know some more things about you, which is necessary for our survey. Please take some time to answer the following questions (it's totally anonymous).</p>""",
+
+                "step1" : "<p>%s</p><img src='/static/%s.jpg' />" % (step1_select(), imgdir),
+
+                "step2" : """<p><span class='underline'>This is the most important part of the work!</span><br/><br/>These are the images that you have selected in Step1. <span class='underline'>Drag the photos and arrange them into a sequence</span> (slideshow) that you think is good for describing the person depicted. Then explain in the textbox why you selected this particular sequence of photos.<br/><br/> We expect you to spend 5 minutes in this task. <br/><br/><span class='underline'>The quality of your work in this stage will determine your paiment and your bonus! All results will be individually analysed. Good quality of work and committeed workers will get a bonus of up to 0.2$ (total 0.4$).</span></p>""",
+
+                "done" : """<p>Thank you for your participation in our slideshow survey!</p>""",
+                }
+
+        # Collect images
+        imgs = [("/static/%s/%s" % (imgdir, i), "/static/%s/large/%s" % 
+            (imgdir, i)) for i in os.listdir("static/%s/" % imgdir) if 
+            i[-4:].lower() == ".jpg"]
+        # Shuffle images to avoid bias			
+        random.shuffle(imgs)
+        return self.render.survey(imgs, sections, survey_form)
 
 
     def POST(self) :
-        pass
+        # Get data and decode it
+        data = json.loads(web.data())
+
+        # Make sure that the data is acceptable (what needs to be done here?)
+
+        # Get data
+        questions = "[%s]" % "; ".join(map(lambda d : str(d['value']), data['questions']))
+        photo_order = "[%s]" % "; ".join(map(str,data['photo_order']))
+			
+        time_diffs = map(lambda td : "(%s: %s)" % (td['name'], td['time']), data['time_diffs'][1:])
+        window_dimensions = [data['window_x'], data['window_y']]
+        mw_id = data['mw_id']
+
+        # Create voucher code
+        #id = self.getRandPath("codes/")
+        if mw_id != "" :
+            salt = "ICECREAM"
+            project = "slideshow"
+            id_hash = hashlib.sha224(mw_id + project + salt).hexdigest()
+        else :
+            id_hash = "Invalid worker id"
+
+        # Create file to mark voucher as used
+        # open("codes/%s" % id, 'a').close()
+
+        # Write data to csv
+        with open('data.csv', 'ab') as f:
+            writer = csv.writer(f)
+            writer.writerow([mw_id, time.strftime("%c"), id_hash, str(data['collection']), photo_order, questions, "[%s]" % "; ".join(time_diffs), str(data['clicks']), "'%s'" % data['question_pick'].replace("\n"," "), "'%s'" % data['question_order'].replace("\n"," "), str(window_dimensions)])       # stringify data and save to file
+        return id_hash
 
 
-    # Switch between folders on each reload
-    def cycleFolder() :
-        self.currentFolderIndex = (self.currentFolderIndex + 1) % 3
-        folders = ["Beach", "Bird", "Other"]
-        return "images/%s/" % folders[self.currentFolderIndex]
+    def getRandPath(self, folder, size=6, chars=ascii_uppercase + digits) :
+        condition = True
+        while condition:
+            name = ''.join(random.choice(chars) for x in range(size))
+            condition = os.path.isdir(folder + name)
+        return name
 
 # Set up web page text fields
-class text_object(object):
+class page_object(object):
     pass
+
+
+# Switch between folders on each reload
+currentFolderIndex = 0
+def cycleFolder() :
+    global currentFolderIndex
+    folders = ["female", "girl", "male", "couple", "baby"]
+    currentFolderIndex = (currentFolderIndex + 1) % len(folders)
+    return "images/%s" % folders[currentFolderIndex]
+
